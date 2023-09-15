25fe547025b0270e7d05b6fb9c4c69cea71e7cb1
Thiago Moraes <thiago.moraes@luizalabs.com>
2023-09-14 21:04:47 -0300

    Atualizar porta 3000

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const express = require ('express')
 const cors = require('cors');
 const path = require ('path')
 const app = express ()
+const apiRouter = require('./api/routes/apiRouter')
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use ('/api', apiRouter)
 
 app.use('/app', express.static (path.join (__dirname, '/public')))
 
-let port = process.env.PORT || 3000
-app.listen (port)
+const PORT = process.env.PORT || 3000
+
+app.listen (PORT, function() {
+    console.log('Servidor rodando na porta 3000')
+})
