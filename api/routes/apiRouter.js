a871626eb9a1ca052d54502a71c598afd103ab31
Thiago Moraes <thiago.moraes@luizalabs.com>
2023-09-14 23:06:28 -0300

    Implementando Crud

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -11,13 +11,7 @@ const knex = require('knex')({
 
 let apiRouter = express.Router()
 const endpoint = '/'
-const lista_produtos = {
-    produtos: [
-        { id: 1, descricao: "Produto 1", valor: 5.00, marca: "marca " },
-        { id: 2, descricao: "Produto 2", valor: 5.00, marca: "marca " },
-        { id: 3, descricao: "Produto 3", valor: 5.00, marca: "marca " },
-    ]
-}
+
 
 apiRouter.get(endpoint + 'animal', (req, res) => {
     knex.select('*').from('animal.animal')
@@ -28,8 +22,60 @@ apiRouter.get(endpoint + 'animal', (req, res) => {
     })
 })
 
-apiRouter.get(endpoint + 'animal/:id', (req, res) => { })
-apiRouter.post(endpoint + 'animal', (req, res) => { })
-apiRouter.put(endpoint + 'animal/:id', (req, res) => { })
-apiRouter.delete(endpoint + 'animal/:id', (req, res) => {})
+apiRouter.get(endpoint + 'animal/:id', (req, res) => { 
+    const id = parseInt (req.params.id)
+    knex.select('*').from('animal.animal where id =' + id)
+    .then( animal => res.status(200).json(animal) )
+    .catch(err => {
+    res.status(500).json({
+    message: 'Erro ao recuperar animais - ' + err.message })
+    })
+
+})
+
+apiRouter.post(endpoint + 'animal', (req, res) => { 
+    const { descricao, preco, raca } = req.body;
+    knex('animal')
+        .insert({ descricao, preco, raca })
+        .then(result => {
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+})
+
+apiRouter.put(endpoint + 'animal/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { descricao, preco, raca } = req.body;
+    knex('animal')
+        .where('id', id)
+        .update({ descricao, preco, raca })
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+ })
+
+apiRouter.delete(endpoint + 'animal/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    knex('animal')
+        .where('id', id)
+        .del()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Um erro ocorreu: ' + err.message,
+            });
+        });
+})
+
 module.exports = apiRouter;
