af5a51d438816b9b46ba5604701c7524a7cac284
Dani Bonfim <daniela@superopa.com>
2023-09-21 15:07:55 -0300

    add header para permissão inline

--- routes/screens/screensRouter.js
+++ routes/screens/screensRouter.js
@@ -2,6 +2,11 @@ const express = require ('express');
 const screensRouter = express.Router();
 const fs = require("fs");
 
+screensRouter.use('/', (req, res, next)=>{
+    res.set('Content-Security-Policy', "default-src * 'unsafe-inline' 'unsafe-eval';")
+    next()
+})
+
 screensRouter.get("/", (req,res) =>{
     const path = require('path').resolve(__dirname, './statics/list-animals.html');
     const file = fs.readFileSync(path,  { encoding: 'utf8'})
